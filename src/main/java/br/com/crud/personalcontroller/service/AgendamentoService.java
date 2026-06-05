73444aa8b02d86f437107cfbffb7086715132193
Vitor Ragazzi Moreira <168283150+VitorRagazzi@users.noreply.github.com>
2026-06-05 16:32:57 -0300

    feat: implementacao de interface agendamentoService

--- src/main/java/br/com/crud/personalcontroller/service/AgendamentoService.java
+++ src/main/java/br/com/crud/personalcontroller/service/AgendamentoService.java
@@ -1,4 +1,12 @@
 package br.com.crud.personalcontroller.service;
 
+import br.com.crud.personalcontroller.database.model.dto.AlunosDTO;
+
+import java.util.List;
+
 public interface AgendamentoService {
+    AlunosDTO salvarAgendamento(AlunosDTO alunosDTO);
+    AlunosDTO buscarPorId(Integer id);
+    List<AlunosDTO> listarAgendamentos();
+    void excluirAgendamento(Integer id);
 }
