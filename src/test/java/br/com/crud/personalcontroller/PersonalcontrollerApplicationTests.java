e81e80cf14573a63b3583b72e6c2591b8ab57c76
Vitor Ragazzi Moreira <168283150+VitorRagazzi@users.noreply.github.com>
2026-06-05 13:29:47 -0300

    Initial commit

---
+++ src/test/java/br/com/crud/personalcontroller/PersonalcontrollerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.crud.personalcontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalcontrollerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
